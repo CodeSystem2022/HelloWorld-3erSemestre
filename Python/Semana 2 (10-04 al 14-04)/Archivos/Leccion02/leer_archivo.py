9e7b79bd6f389dd7abc5877af3806557ee5e2d25
MacaMorales <macarena.l.morales16@gmail.com>
2023-04-26 18:45:10 -0300

    Semana 2- Lección 02 en Python

---
+++ Python/Semana 2 (10-04 al 14-04)/Archivos/Leccion02/leer_archivo.py
@@ -0,0 +1,7 @@
+
+archivo = open("prueba.txt", "r", encoding="utf8")
+# print(archivo.read())
+# print(archivo.read(16))
+# print(archivo.read(16)) # Continuamos desde la linea anterior
+print(archivo.readline())
+
---
+++ Python/Semana 2 (10-04 al 14-04)/Archivos/Leccion02/manejo_archivos.py
@@ -0,0 +1,14 @@
+# Declaramos una variable
+try:
+    archivo = open("prueba.txt", "w", encoding="utf8") # La w es de write que significa escribir
+    archivo.write("Programamos con diferentes tipos de archivos, ahora en txt.\n")
+    archivo.write("Los acentos son importantes para las palabras\n")
+    archivo.write("Como por ejemplo: acción, ejecución y producción\n")
+    archivo.write("las letras son:\nr read = leer\na append = anexa\nw write = escribe\nx = crea un archivo")
+    archivo.write("\nt esta es para texto o text,\nb archivos binarios,\nw+ lee y escribe son iguales r+\n")
+    archivo.write("Con esto terminamos")
+except Exception as e:
+    print(e)
+finally: #Siempre se ejecuta
+    archivo.close() # Con esto se debe cerrar el archivo
+#  archivo.write(" Todo quedo perfecto"): este es un error
