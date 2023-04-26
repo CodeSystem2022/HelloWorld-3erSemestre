b9fe43f12a0d7ab4531b4cfed861a11376297e74
MacaMorales <macarena.l.morales16@gmail.com>
2023-04-26 18:34:03 -0300

    Semana 1-Leccion 01 en Java
    Se agregó la Lección 01 dentro de la Carpeta de Java, trabajada en la semana 1.

---
+++ Java/Semana 1 (03-04 al 07-04)/Leccion01/MundoPc/src/mundopc/mundoPC.java
@@ -0,0 +1,33 @@
+
+package mundopc;
+
+import ar.com.system2023.mundopc.*;
+
+public class mundoPC {
+    public static void main(String[] args) {
+        Monitor monitorHP = new Monitor("HP", 13);//Importar la clase
+        Teclado tecladoHP = new Teclado("Bluetooth", "HP");
+        Raton ratonHP = new Raton("Bluetooh", "HP");
+        Computadora computadoraHP = new Computadora("Computadora HP", monitorHP, tecladoHP, ratonHP);
+        
+        //Creamos otros objetos de diferente marca
+        Monitor monitorGamer = new Monitor("Gamer", 32);
+        Teclado tecladoGamer = new Teclado("Bluetooth", "Gamer");
+        Raton ratonGamer = new Raton("Bluetooh", "Gamer");
+        Computadora computadoraGamer = new Computadora("Computadora Gamer", monitorGamer, tecladoGamer, ratonGamer);
+        Orden orden1 = new Orden();//Inicializamos el arreglo vacio
+        Orden orden2 = new Orden();//Una nueva lista para el objeto orden2
+        orden1.agregarComputadora(computadoraHP);
+        orden1.agregarComputadora(computadoraGamer);
+        
+        Computadora computadorasVarias = new Computadora("Computadora de diferentes marcas", monitorHP, tecladoGamer, ratonHP);
+        orden2.agregarComputadora(computadorasVarias);
+        
+        orden1.mostrarOrden();
+        orden2.mostrarOrden();
+       
+        //Crear mas objetos de tipo computadora con todos sus elementos
+        //completar una lista en ele objeto orden1 que llegue a los 10 elementos
+        //probar de esta manera los metodos al maximo rendimiento
+    }
+}
